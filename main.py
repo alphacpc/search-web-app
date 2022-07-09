13d0785ee2a12d5f9c71f72a5715e67585fccce6
alphacpc <alphacpc@gmail.com>
2022-07-09 14:43:50 +0000

    Updated Transform data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from pprint import pprint
 
 def get_airline(code):
     with open("airlines.csv", 'r') as file:
@@ -30,15 +31,15 @@ with open("flights.csv", 'r') as file:
 
     for row in csvreader:
 
-        # print("-------HEURE DE DEPART---------")
-        # time_dep_prevu = row['SCHEDULED_DEPARTURE'][:2] + ":" + row['SCHEDULED_DEPARTURE'][2:]
-        # time_dep = row['DEPARTURE_TIME'][:2] + ":" + row['DEPARTURE_TIME'][2:]
+        print("-------HEURE DE DEPART---------")
+        time_dep_prevu = row['SCHEDULED_DEPARTURE'][:2] + ":" + row['SCHEDULED_DEPARTURE'][2:]
+        time_dep = row['DEPARTURE_TIME'][:2] + ":" + row['DEPARTURE_TIME'][2:]
        
-        # print("-------HEURE D'ARRIVEE---------")
-        # time_arr_prevu = row['SCHEDULED_ARRIVAL'][:2] + ":" + row['SCHEDULED_ARRIVAL'][2:]
-        # time_arr = row['ARRIVAL_TIME'][:2] + ":" + row['ARRIVAL_TIME'][2:]
+        print("-------HEURE D'ARRIVEE---------")
+        time_arr_prevu = row['SCHEDULED_ARRIVAL'][:2] + ":" + row['SCHEDULED_ARRIVAL'][2:]
+        time_arr = row['ARRIVAL_TIME'][:2] + ":" + row['ARRIVAL_TIME'][2:]
 
-        # print("-------VOL INFOS---------")
+        print("-------VOL INFOS---------")
         # name_airline = get_airline(row['AIRLINE'])['AIRLINE']
         # image_airline = get_airline(row['AIRLINE'])['IMAGE']
         # origin_airport = get_airport(row['ORIGIN_AIRPORT'])['CITY']
@@ -50,19 +51,20 @@ with open("flights.csv", 'r') as file:
 
         flight = {
             "NUM_FLIGHT" : row['FLIGHT_NUMBER'],
-            "DEPART_PREVU" : time_dep_prevu, 
-            "DEPART" : time_dep,
-            "ARRIVE_PREVU" : time_arr_prevu, 
-            "ARRIVE" : time_arr, 
-            "DATE" : flight_date, 
-            "DATE_FORMATED" : flight_date_formated, 
-            "AIRLINE" : name_airline, 
-            "AIRLINE_PHOTO" : image_airline, 
-            "ORIGIN_AIRPORT" : origin_airport, 
-            "DESTINATION_AIRPORT" : destination_airport,
+            "DEPART_PREVU" : row['SCHEDULED_DEPARTURE'][:2] + ":" + row['SCHEDULED_DEPARTURE'][2:], 
+            "DEPART" : row['DEPARTURE_TIME'][:2] + ":" + row['DEPARTURE_TIME'][2:],
+            "ARRIVE_PREVU" : row['SCHEDULED_ARRIVAL'][:2] + ":" + row['SCHEDULED_ARRIVAL'][2:], 
+            "ARRIVE" : row['ARRIVAL_TIME'][:2] + ":" + row['ARRIVAL_TIME'][2:], 
+            "DATE" : "{}.{}.{}".format(row['DAY'], row['MONTH'], row['YEAR']), 
+            "DATE_FORMATED" : "Le {} {}, {}".format(row['DAY'], get_month(row['MONTH']), row['YEAR']), 
+            "AIRLINE" : get_airline(row['AIRLINE'])['AIRLINE'], 
+            "AIRLINE_PHOTO" : get_airline(row['AIRLINE'])['IMAGE'], 
+            "ORIGIN_AIRPORT" : get_airport(row['ORIGIN_AIRPORT'])['CITY'], 
+            "DESTINATION_AIRPORT" : get_airport(row['DESTINATION_AIRPORT'])['CITY'],
             "DISTANCE" : row['DISTANCE']
         }
 
+        pprint(flight)
         
 
         break
