8bc67d741cacb99ebd637682d9ede47770b9a585
alphacpc <alphacpc@gmail.com>
2022-07-09 20:51:34 +0000

    Updated transform data file

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ flights = []
 flights_fields = ["NUM_FLIGHT", "DEPART_PREVU", "DEPART", "ARRIVE_PREVU", "ARRIVE", "DATE" , "DATE_FORMATED", "AIRLINE", "AIRLINE_PHOTO", "ORIGIN_AIRPORT", "DESTINATION_AIRPORT" , "DISTANCE"]
 
 def get_airline(code):
-    with open("airlines.csv", 'r') as file:
+    with open("./data/airlines.csv", 'r') as file:
         csvreader = csv.DictReader(file)
         return [item for item in csvreader if item.get('IATA_CODE') == code][0]
 
 
 def get_airport(airport):
-    with open("airports.csv", 'r') as file:
+    with open("./data/airports.csv", 'r') as file:
         csvreader = csv.DictReader(file)
-        return [item for item in csvreader if item.get('IATA_CODE') == airport][0]
+        
+        result = [item for item in csvreader if item.get('IATA_CODE') == airport]
+
+        return result[0] if len(result) > 0 else None
+        # return [item for item in csvreader if item.get('IATA_CODE') == airport][0]
 
 
 def get_month(num):
@@ -28,11 +32,13 @@ def get_month(num):
 
 
 
-with open("flights.csv", 'r') as file:
+with open("./data/flights.csv", 'r') as file:
     csvreader = csv.DictReader(file)
 
-
     for row in csvreader:
+
+        origin_airport = get_airport(row['ORIGIN_AIRPORT'])
+        destination_airport = get_airport(row['DESTINATION_AIRPORT'])
 
         flight = {
             "NUM_FLIGHT" : row['FLIGHT_NUMBER'],
@@ -44,13 +50,16 @@ with open("flights.csv", 'r') as file:
             "DATE_FORMATED" : "Le {} {}, {}".format(row['DAY'], get_month(row['MONTH']), row['YEAR']), 
             "AIRLINE" : get_airline(row['AIRLINE'])['AIRLINE'], 
             "AIRLINE_PHOTO" : get_airline(row['AIRLINE'])['IMAGE'], 
-            "ORIGIN_AIRPORT" : get_airport(row['ORIGIN_AIRPORT'])['CITY'], 
-            "DESTINATION_AIRPORT" : get_airport(row['DESTINATION_AIRPORT'])['CITY'],
+            "ORIGIN_AIRPORT" : origin_airport.get('CITY') if origin_airport != None else None,
+            "DESTINATION_AIRPORT" : destination_airport.get('CITY') if destination_airport != None else None,
             "DISTANCE" : row['DISTANCE']
         }
 
         flights.append(flight)
-        
+
+        break
+
+
 
 with open("flights_transformed.csv", "w", encoding="UTF8", newline="") as file:
     writer = csv.DictWriter(file, fieldnames = flights_fields)
