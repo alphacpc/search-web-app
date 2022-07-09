1a84b9153dda84a0402055ac44d752c41d0b0abc
alphacpc <alphacpc@gmail.com>
2022-07-09 14:12:31 +0000

    Updated transform data

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+from elasticsearch6 import Elasticsearch
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://groupe3:passer123@localhost/chickens'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+elastic = Elasticsearch()
 
 @app.route('/', methods=["GET"])
 def home():
+    
+    resp = elastic.get(index='csv', id='7amA3oEBF_mbnRB09AuW', doc_type='_doc').get('_source')
+    print(resp)
+    
+
     return render_template('home.html')
 
 
--- main.py
+++ main.py
@@ -1,50 +1,63 @@
 import csv
-rows = []
+
+def get_airline(code):
+    with open("airlines.csv", 'r') as file:
+        csvreader = csv.DictReader(file)
+        return [item for item in csvreader if item.get('IATA_CODE') == code][0]
+
+
+def get_airport(airport):
+    with open("airports.csv", 'r') as file:
+        csvreader = csv.DictReader(file)
+        return [item for item in csvreader if item.get('IATA_CODE') == airport][0]
+
+def get_month(num):
+    months = [
+        {"id": 1, "month": "Janvier"}, {"id": 2, "month": "Février"},
+        {"id": 3, "month": "Mars"}, {"id": 4, "month": "Avril"},
+        {"id": 5, "month": "Mais"}, {"id": 6, "month": "Juin"},
+        {"id": 7, "month": "Juillet"}, {"id": 8, "month": "Aout"},
+        {"id": 9, "month": "Septembre"}, {"id": 10, "month": "Octobre"},
+        {"id": 11, "month": "Novembre"}, {"id": 12, "month": "Décembre"},
+    ]
+    return [month for month in months if month.get('id') == int(num)][0]['month']
+
+
+
 with open("flights.csv", 'r') as file:
     csvreader = csv.DictReader(file)
     for row in csvreader:
 
         print("-------HEURE DE DEPART---------")
-
         heures_dep_prevu = row['SCHEDULED_DEPARTURE'][:2]
         minutes_dep_prevu = row['SCHEDULED_DEPARTURE'][2:]
         heures_dep = row['DEPARTURE_TIME'][:2]
         minutes_dep = row['DEPARTURE_TIME'][2:]
-
-        print("Heure de départ prévu", heures_dep_prevu, ":", minutes_dep_prevu)
-        print("Heure de départ", heures_dep_prevu, ":", minutes_dep_prevu)
-
-        
-        print("-------HEURE D'ARRIVE---------")
-
-
+       
+        print("-------HEURE D'ARRIVEE---------")
         heures_arr_prevu = row['SCHEDULED_ARRIVAL'][:2]
         minutes_arr_prevu = row['SCHEDULED_ARRIVAL'][2:]
+        time_arr_prevu = row['SCHEDULED_ARRIVAL'][:2] + ":" + row['SCHEDULED_ARRIVAL'][2:]
+        print(time_arr_prevu)
+
         heures_arr = row['ARRIVAL_TIME'][:2]
         minutes_arr = row['ARRIVAL_TIME'][2:]
+        time_arr = row['ARRIVAL_TIME'][:2] + ":" + row['ARRIVAL_TIME'][2:]
 
-        print("Heure d'arrivé prévu", heures_arr_prevu, ":", minutes_arr_prevu)
-        print("Heure d'arrivé", heures_arr, ":", minutes_arr)
+        print(time_arr)
 
-        print("\n")
+        print("-------VOL INFOS---------")
+        name_airline = get_airline(row['AIRLINE'])['AIRLINE']
+        image_airline = get_airline(row['AIRLINE'])['IMAGE']
+        origin_airport = get_airport(row['ORIGIN_AIRPORT'])['CITY']
+        destination_airport = get_airport(row['DESTINATION_AIRPORT'])['CITY']
+
+        month = get_month(row['MONTH'])
+        distance = row['DISTANCE']
+        year = row['YEAR']
+        day = row['DAY']
+        filght_number = row['FLIGHT_NUMBER']
+
+
+
         break
-
-
-def get_aita_code_compagny(code):
-    
-    compagnies = [
-        {"code": "UA", "airline": "United Air Lines Inc" , "image": "./images/united.png" },
-        {"code": "AA", "airline": "American Airlines Inc"  , "image": "./images/american-airlines.png" },
-        {"code": "US", "airline": "US Airways Inc"  , "image": "./images/us.png" },
-        {"code": "F9", "airline": "Frontier Airlines Inc" , "image": "./images/frontier.png" },
-        {"code": "B6", "airline": "JetBlue Airways" , "image": "./images/jetblue.png" },
-        {"code": "OO", "airline": "Skywest Airlines Inc" , "image": "./images/sktywest.png" },
-        {"code": "AS", "airline": "Alaska Airlines Inc" , "image": "./images/alaska.png" },
-        {"code": "NK", "airline": "Spirit Air Lines" , "image": "./images/spirit.png" },
-        {"code": "WN", "airline": "Southwest Airlines Co" , "image": "./images/southwest.png" },
-        {"code": "DL", "airline": "Delta Air Lines Inc" , "image": "./images/delta.png" },
-        {"code": "EV", "airline": "Atlantic Southeast Airlines" , "image": "./images/southwest.png" },
-        {"code": "HA", "airline": "Hawaiian Airlines Inc" , "image": "./images/hawa.png" },
-        {"code": "MQ", "airline": "American Eagle Airlines Inc" , "image": "./images/american-airlines.png" },
-        {"code": "VX", "airline": "Virgin America" , "image": "./images/virgini.png" }
-    ]
